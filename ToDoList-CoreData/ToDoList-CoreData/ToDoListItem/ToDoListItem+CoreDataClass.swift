37f7b64cc25913a7581270a679b51714342d007f
Emilia Nedyalkova <emilia_nedialkova@abv.bg>
2021-05-19 23:59:07 +0300

    added To Do List Core Data

---
+++ ToDoList-CoreData/ToDoList-CoreData/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoList-CoreData
+//
+//  Created by Emilia Nedyalkova on 19.05.21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ ToDoList-CoreData/ToDoList-CoreData/ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  ToDoList-CoreData
+//
+//  Created by Emilia Nedyalkova on 19.05.21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
